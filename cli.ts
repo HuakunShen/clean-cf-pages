d7d3bdecb60af1810c2996fb11b0cfb8ddebf2db
Huakun Shen <huakun.shen@huakunshen.com>
2025-01-06 13:14:55 -0500

    Refactor CLI to support environment variables for API token, account ID, and project name; make arguments optional

--- cli.ts
+++ cli.ts
@@ -3,6 +3,11 @@ import { defineCommand, runMain } from "citty";
 import { run } from "./lib";
 import { string, parse } from "valibot";
 
+// Environment variables
+const ENV_API_TOKEN = process.env.CF_API_TOKEN;
+const ENV_ACCOUNT_ID = process.env.CF_ACCOUNT_ID;
+const ENV_PROJECT_NAME = process.env.CF_PROJECT_NAME;
+
 const main = defineCommand({
   meta: {
     name: "Cleann CF Pages",
@@ -11,15 +16,15 @@ const main = defineCommand({
   },
   args: {
     apiToken: {
-      required: true,
+      required: false,
       description: "Cloudflare API Token",
     },
     accountId: {
-      required: true,
+      required: false,
       description: "Cloudflare Account ID",
     },
     projectName: {
-      required: true,
+      required: false,
       description: "Cloudflare Pages project name",
     },
     deleteAliasedDeployments: {
@@ -29,10 +34,22 @@ const main = defineCommand({
     },
   },
   run({ args }) {
+    const apiToken = args.apiToken || ENV_API_TOKEN;
+    const accountId = args.accountId || ENV_ACCOUNT_ID;
+    const projectName = args.projectName || ENV_PROJECT_NAME;
+    if (!apiToken) {
+      throw new Error("API token is required");
+    }
+    if (!accountId) {
+      throw new Error("Account ID is required");
+    }
+    if (!projectName) {
+      throw new Error("Project name is required");
+    }
     run(
-      parse(string(), args.apiToken),
-      parse(string(), args.accountId),
-      parse(string(), args.projectName),
+      parse(string(), apiToken),
+      parse(string(), accountId),
+      parse(string(), projectName),
       args.deleteAliasedDeployments
     );
   },
